ebbed59ff4c9304934074172014fa0e34bf1d0f3
Ofek Cohen <Ofekcohen3300@gmail.com>
2025-05-20 15:18:27 +0300

    i try to fix duration but i doesnt work so i delete the duration changes

--- breakoutGame/breakout_game.h
+++ breakoutGame/breakout_game.h
@@ -105,7 +105,6 @@ namespace breakout {
 
     struct BreakAnimation {
         float timer = 0.0f; //seconds
-        float duration = 0.5f; // default
     };
 
     //----------------------------------
